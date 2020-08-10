0dd2418800cf7b3555ccd4fbd0075bafa18b584d
ellomdian <31892519+ellomdian@users.noreply.github.com>
2020-08-10 10:23:49 -0600

    Update switch-child-device.groovy
    * Established dev line for code modification.
    * Removed spaces in device name for better response.
    * General cleanup.

--- Drivers/switch-child-device.src/switch-child-device.groovy
+++ Drivers/switch-child-device.src/switch-child-device.groovy
@@ -1,7 +1,8 @@
+// JavaScript source code
 /**
  *  Switch Child Device
  *
- *  Copyright 2017 Eric Maycock
+ *  Copyright 2020 Josh Ulmer (original Code by Eric Maycock)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,25 +15,11 @@
  *
  */
 metadata {
-	definition (name: "Switch Child Device", namespace: "erocm123", author: "Eric Maycock") {
+	definition (name: "SwitchChildDeviceDev", namespace: "joshu", author: "Josh Ulmer") {
 		capability "Switch"
 		capability "Actuator"
 		capability "Sensor"
         capability "Refresh"
-	}
-
-	tiles {
-		multiAttributeTile(name:"switch", type: "lighting", width: 3, height: 4, canChangeIcon: true){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"turningOn"
-				attributeState "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState:"turningOff"
-				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#00A0DC", nextState:"turningOff"
-				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
-			}
-		}
-        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-		    state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
-        }
 	}
 }
 
